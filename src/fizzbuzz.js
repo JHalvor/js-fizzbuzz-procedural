8da9074264300b2631acf43e187dd6de35f3e375
Jonas <jonashalvorsen2013@live.no>
2024-09-23 08:43:44 +0200

    Exercise-complete

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,7 +1,18 @@
+const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
 const answer = []
 
 // Write your code below this line
-
+numbers.forEach(element => {
+    if (element % 15 == 0) {
+        answer.push('FizzBuzz')
+    } else if (element % 5 == 0) {
+        answer.push('Buzz')
+    } else if (element % 3 == 0) {
+        answer.push('Fizz')
+    } else {
+        answer.push(element)
+    }
+});
 
 
 
